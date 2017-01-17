532b0f0b00656f0508b84799226c02fdd171567e
Miika Tiihonen <K9348@student.jamk.fi>
2017-01-17 17:09:49 +0200

    Tehtävä1 tehty

---
+++ BlackJackV0/Viikkotehtavat/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tehtava1();
+        }
+
+        static void Tehtava1()
+        {
+            //Tee ohjelma, joka tulostaa kayttajan antamaa lukua (1, 2 tai 3) vastaavan luvun tekstina
+            Console.WriteLine("Anna 1, 2 tai 3 ja tulostan sen tekstina");
+            string retval = Console.ReadLine();
+            int luku = int.Parse(retval);
+            if (luku == 1)
+            {
+                retval = "yksi";
+            }
+            else if (luku == 2)
+            {
+                retval = "kaksi";
+            }
+            else if (luku == 3)
+            {
+                retval = "kolme";
+            }
+            else
+            {
+                retval = "joku muu luku";
+            }
+            Console.WriteLine(retval);
+        }
+    }
+}
